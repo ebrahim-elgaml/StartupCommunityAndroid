6f26fa66703c70dff380a166f5bf6b528c6dd4b0
ebrahim-elgaml <ebrahim.el.gaml@gmail.com>
2015-12-02 18:49:05 +0200

    add adapter

---
+++ app/src/main/java/localhost3000/startupcommunity/dummy/FriendRequestList.java
@@ -0,0 +1,103 @@
+package localhost3000.startupcommunity.dummy;
+
+import android.app.Activity;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import localhost3000.startupcommunity.R;
+
+
+public class FriendRequestList extends ArrayAdapter<String>{
+    public static List<SingleRequest> items = new ArrayList<SingleRequest>();
+    private Activity context;
+    PlayToastAlert soundToastAlert;
+    public FriendRequestList(Activity context, List<SingleRequest> l, List<String> requestId) {
+        super(context, R.layout.single_friend_request, requestId);
+        this.items = l;
+        this.context = context;
+        this.soundToastAlert = (PlayToastAlert)context;
+    }
+
+    private static void addItem(SingleRequest item) {
+        items.add(item);
+    }
+
+    @Override
+    public Activity getContext() {
+        return context;
+    }
+    public View getView(int i, View view, ViewGroup viewGroup) {
+
+        SingleRequest request = (SingleRequest)items.get(i);
+        if (view == null) // reuse existing view
+            view = context.getLayoutInflater().inflate(R.layout.single_friend_request,
+                    viewGroup, false);
+        // Set the TextView to the name of the sound
+        TextView t = (TextView)view.findViewById(R.id.list_item_string);
+        t.setText(request.getName());
+
+        // Set the tag of the button to the sound resource id (uri)
+        Button b = (Button)view.findViewById(R.id.list_item_button);
+        b.setTag(request.getRequestId());
+        b.setOnClickListener(this);
+        return view;
+    }
+    public void onClick(View view) {
+        Button b = (Button) view;
+        if (b != null) {
+            int buttonID = (int)b.getTag();
+            if (soundToastAlert != null) {
+                soundToastAlert.playToast(id);
+            }
+        }
+    }
+    public interface PlayToastAlert {
+        public void playToast(int id);
+    }
+
+    public void setContext(Activity context) {
+        this.context = context;
+    }
+
+    public static class SingleRequest {
+        private String name ;
+        private int requestId ;
+        private String imageUrl;
+        public SingleRequest(String name, int request, String imageUrl){
+            this.name = name;
+            this.requestId = request;
+            this.imageUrl = imageUrl;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public String getImageUrl() {
+            return imageUrl;
+        }
+
+        public void setImageUrl(String imageUrl) {
+            this.imageUrl = imageUrl;
+        }
+
+        public int getRequestId() {
+            return requestId;
+        }
+
+        public void setRequestId(int requestId) {
+            this.requestId = requestId;
+        }
+
+    }
+}
